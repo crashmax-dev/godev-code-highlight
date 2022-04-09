38d39113fe0ae5aa46758ae728504c751214bc07
crashmax-dev <crashmax-dev@users.noreply.github.com>
2022-04-09 08:48:55 +0000

    Deploying to gh-pages from @ crashmax-dev/godev-code-highlight@35aa170f22847331b2c59e73d4e54ad3de14a7e5 🚀

---
+++ godev-code-highlight.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        godev-code-highlight
+// @version     0.0.1
+// @author      Vitalij Ryndin
+// @description godev-code-highlight
+// @homepage    https://crashmax-off.github.io/godev-code-highlight/
+// @include     /^(http|https)://go.dev.*$/
+// ==/UserScript==
--- webpack-userscript-template.meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        webpack-userscript-template
-// @version     0.0.1
-// @author      Vitalij Ryndin
-// @description webpack-userscript-template
-// @homepage    https://crashmax-off.github.io/webpack-userscript-template/
-// @include     /^(http|https)://(example.com|example.org|example.edu).*$/
-// ==/UserScript==
