9307723b6f3ff29553f4107a3051240a844bff80
crashmax-dev <crashmax.dev@gmail.com>
2022-09-06 13:15:00 +0000

    Deploying to gh-pages from @ crashmax-dev/godev-code-highlight@ba2903db51bfaf836d66bb0158f465e2b75e53f3 🚀

---
+++ godev-code-highlight.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        godev-code-highlight
+// @description godev-code-highlight
+// @version     2.0.0
+// @author      Vitalij Ryndin
+// @match       https://go.dev/doc/*
+// @grant       GM_addStyle
+// ==/UserScript==
